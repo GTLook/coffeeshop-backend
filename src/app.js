471c5d86457c94f84383fc1b8b695789b7d5216c
Mark Pavlovski <markpavlovski@gmail.com>
2018-04-25 22:04:34 -0700

    brought in all of the info into app.js other than routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,39 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+const express = = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const authController = require('./controllers/auth')
 
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').load()
+}
+
+const app = express()
+
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+
+
+//  Routes //
+
+
+
+
+
+
+
+
+
+
+//  Sockets & Serve//
+
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
